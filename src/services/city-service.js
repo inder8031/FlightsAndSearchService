28be91706a3df18abb1cbd3909435441aea9908d
unknown <inderece8031@gmail.com>
2024-07-23 17:29:56 +0530

    completed city services

---
+++ src/services/city-service.js
@@ -0,0 +1,49 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+    constructor () {
+        this.cityService = new CityRepository();
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityService.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in city service.");
+            throw { error };
+        }
+    }
+
+    async createCity({ name }) {
+        try {
+            const city = await this.cityService.createCity({ name });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in city service.");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityService.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in city service.");
+            throw { error };
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const res = await this.cityService.deleteCity(cityId);
+            return res;
+        } catch (error) {
+            console.log("Something went wrong in city service.");
+            throw { error };
+        }
+    }
+}
+
+module.exports = CityService;
