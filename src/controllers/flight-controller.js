c2e77ffd481598e3c5ea8d92093b52eee7ccfe1d
unknown <inderece8031@gmail.com>
2024-07-29 18:55:06 +0530

    filtering necessary properties from request body, to prevent bloating of request body

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,16 @@ const flightService = new FlightService();
 
 const create = async (req, res) => {
     try {
-        const flight = await flightService.createFlight(req.body);
+        const flightReqData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            departureTime: req.body.departureTime,
+            arrivalTime: req.body.arrivalTime,
+            price: req.body.price
+        }
+        const flight = await flightService.createFlight(flightReqData);
         return res.status(201).json({
             data: flight,
             success: true,
