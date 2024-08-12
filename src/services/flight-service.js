81721eee5aff2e98bf7b7fe33034b55ef75459ad
unknown <inderece8031@gmail.com>
2024-08-13 01:55:22 +0530

    Added update code in service layer

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -89,6 +89,16 @@ class FlightService {
             throw { error };
         }
     }
+
+    async updateFlight(flightId, data) {
+        try {
+            const flight = await this.flightRepository.updateFlight(flightId, data);
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong in flight service layer");
+            throw { error };
+        }
+    }
 }
 
 module.exports = FlightService;
